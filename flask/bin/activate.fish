c5882b90052a82e6ed8bf6d7f7f20d089d76c7c5
Seif Allah <saifeldarageely@gmail.com>
2024-08-31 20:49:41 +0300

    Setting env

--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/seif/alx/flask"
+set -gx VIRTUAL_ENV "/home/seif/alx/Youtweet/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
