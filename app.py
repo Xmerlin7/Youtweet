eb503e9b940f1e72cf5063b19bf0f5e377223c6c
Seif Allah <saifeldarageely@gmail.com>
2024-09-01 14:44:57 +0300

    Including url_for library

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """_summary_: all project Imports will be added here
 """
-from  flask import Flask, render_template
+from  flask import Flask, render_template, url_for
 app = Flask(__name__)
 posts = [
     {
