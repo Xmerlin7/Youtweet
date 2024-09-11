132e584aeb51c316d4b9f3023b07785d62242633
Seif Allah <saifeldarageely@gmail.com>
2024-09-11 17:56:10 +0300

    adding error handler

--- youtweet/routes.py
+++ youtweet/routes.py
@@ -160,13 +160,14 @@ def user_posts(username):
     return render_template('user_posts.html', posts=posts, user=user)
 
 
-@app.app_error_handler(404)
+@app.errorhandler(404)
 def page_not_found(error):
     return render_template('errors/404.html'), 404
-@app.app_error_handler(403)
+
+@app.errorhandler(403)
 def forbidden(error):
     return render_template('errors/403.html'), 403
 
-@app.app_error_handler(500)
+@app.errorhandler(500)
 def internal_server_error(error):
     return render_template('errors/500.html'), 500
